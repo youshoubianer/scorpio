cd805cafd03977bfc1d6373c623432e9f62383ac
youshoubianer <you_shoubian@163.com>
2016-10-25 00:27:16 +0800

    mood anaylist

--- src/crawer.js
+++ src/crawer.js
@@ -5,13 +5,30 @@ var Crawer = function(){}
 module.exports = new Crawer()
 
 Crawer.prototype.getPage = function * () {
+  let targetUrl = config.qzoneUrl + '/' + config.qq + '/' + config.mood
   let getPageInfo = yield nightmare
-    .goto(config.firstPageUrl)
-    .wait('.head-info h1 span')
+    .goto(targetUrl)
+    .wait('.layout-background')
+    // .wait(3000)
     .evaluate(function(){
-      var title = document.querySelector('.head-info h1 span').innerHTML
-      console.log('title>>>',title);
-      return title
+      console.log('begin>>>');
+      var frame = window.frames[0].document
+      var feeds = frame.querySelectorAll('.feed'),
+          feed = ''
+      console.log('feeds>>> ',feeds);
+      for(feed of feeds){
+        console.log('feed>>>',feed);
+        console.log('text>>> ',feed.querySelector('.bd .content').innerHTML);
+        console.log('attr>>> ',feed.querySelector('.md a img').src);
+        console.log('time>>> ',feed.querySelector('.ft .info a').innerHTML);
+        console.log('device>>> ',feed.querySelector('.ft .info .custom-tail').innerHTML);
+        // console.log('likes>>> ',feed.querySelectorAll('.qz_like_btn')[1].innerHTML);
+        console.log('评论>>> ',feed.querySelector('.comment_btn').innerHTML);
+        console.log('转发>>> ',feed.querySelector('.forward_btn').innerHTML);
+        // console.log('liker>>> ',feed.querySelector('.feed_like a').href,feed.querySelector('.feed_like a').innerHTML);
+      }
+      
+      return 'title'
     })
     .then(function (result) {
       return result
